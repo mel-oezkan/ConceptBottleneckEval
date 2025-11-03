a441db3f579174d1b43857c0852fd3276d9091cd
mel-oezkan <melih.oezkan@stud.tu-darmstadt.de>
2025-11-03 15:38:38 +0100

    updated run sh

--- run_exp.sh
+++ run_exp.sh
@@ -1,7 +1,7 @@
 python3 experiments.py cub Concept_XtoC \
     --seed 1 -ckpt 1 -log_dir ConceptModel__Seed1/outputs/ \
     -e 1000 -optimizer sgd -pretrained -use_aux -use_attr \
-    -weighted_loss multiple -data_dir ./data/processed \
+    -weighted_loss multiple -data_dir ./class_attr_data_10 \
     -n_attributes 112 -normalize_loss -b 64 -weight_decay 0.00004 \
     -lr 0.01 -scheduler_step 1000 -bottleneck
 
