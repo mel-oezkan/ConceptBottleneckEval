742d70ea1467e164fe1a2105d766e9eb92ec0a68
mel-oezkan <melih.oezkan@stud.tu-darmstadt.de>
2025-11-10 01:04:14 +0100

    refactor: ruff experiments

--- experiments.py
+++ experiments.py
@@ -1,11 +1,9 @@
-
 import pdb
 import sys
 
 
 def run_experiments(dataset, args):
-
-    if dataset == 'OAI':
+    if dataset == "OAI":
         from OAI.train import (
             train_X_to_C,
             train_oracle_C_to_y_and_test_on_Chat,
@@ -16,10 +14,10 @@ def run_experiments(dataset, args):
             train_X_to_Cy,
             train_probe,
             test_time_intervention,
-            hyperparameter_optimization
+            hyperparameter_optimization,
         )
 
-    elif dataset == 'CUB':
+    elif dataset == "CUB":
         from CUB.train import (
             train_X_to_C,
             train_oracle_C_to_y_and_test_on_Chat,
@@ -30,65 +28,75 @@ def run_experiments(dataset, args):
             train_probe,
             test_time_intervention,
             robustness,
-            hyperparameter_optimization
+            hyperparameter_optimization,
         )
 
     experiment = args[0].exp
-    if experiment == 'Concept_XtoC':
+    if experiment == "Concept_XtoC":
         train_X_to_C(*args)
 
-    elif experiment == 'Independent_CtoY':
+    elif experiment == "Independent_CtoY":
         train_oracle_C_to_y_and_test_on_Chat(*args)
 
-    elif experiment == 'Sequential_CtoY':
+    elif experiment == "Sequential_CtoY":
         train_Chat_to_y_and_test_on_Chat(*args)
 
-    elif experiment == 'Joint':
+    elif experiment == "Joint":
         train_X_to_C_to_y(*args)
 
-    elif experiment == 'Standard':
+    elif experiment == "Standard":
         train_X_to_y(*args)
 
-    elif experiment == 'StandardWithAuxC':
+    elif experiment == "StandardWithAuxC":
         train_X_to_y_with_aux_C(*args)
 
-    elif experiment == 'Multitask':
+    elif experiment == "Multitask":
         train_X_to_Cy(*args)
 
-    elif experiment == 'Probe':
+    elif experiment == "Probe":
         train_probe(*args)
 
-    elif experiment == 'TTI':
+    elif experiment == "TTI":
         test_time_intervention(*args)
 
-    elif experiment == 'Robustness':
+    elif experiment == "Robustness":
         robustness(*args)
 
-    elif experiment == 'HyperparameterSearch':
+    elif experiment == "HyperparameterSearch":
         hyperparameter_optimization(*args)
+
 
 def parse_arguments():
     # First arg must be dataset, and based on which dataset it is, we will parse arguments accordingly
-    assert len(sys.argv) > 2, 'You need to specify dataset and experiment'
-    assert sys.argv[1].upper() in ['OAI', 'CUB'], 'Please specify OAI or CUB dataset'
-    assert sys.argv[2] in ['Concept_XtoC', 'Independent_CtoY', 'Sequential_CtoY',
-                           'Standard', 'StandardWithAuxC', 'Multitask', 'Joint', 'Probe',
-                           'TTI', 'Robustness', 'HyperparameterSearch'], \
-        'Please specify valid experiment. Current: %s' % sys.argv[2]
+    assert len(sys.argv) > 2, "You need to specify dataset and experiment"
+    assert sys.argv[1].upper() in ["OAI", "CUB"], "Please specify OAI or CUB dataset"
+    assert sys.argv[2] in [
+        "Concept_XtoC",
+        "Independent_CtoY",
+        "Sequential_CtoY",
+        "Standard",
+        "StandardWithAuxC",
+        "Multitask",
+        "Joint",
+        "Probe",
+        "TTI",
+        "Robustness",
+        "HyperparameterSearch",
+    ], "Please specify valid experiment. Current: %s" % sys.argv[2]
     dataset = sys.argv[1].upper()
     experiment = sys.argv[2].upper()
 
     # Handle accordingly to dataset
-    if dataset == 'OAI':
+    if dataset == "OAI":
         from OAI.train import parse_arguments
-    elif dataset == 'CUB':
+    elif dataset == "CUB":
         from CUB.train import parse_arguments
 
     args = parse_arguments(experiment=experiment)
     return dataset, args
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     import torch
     import numpy as np
 
