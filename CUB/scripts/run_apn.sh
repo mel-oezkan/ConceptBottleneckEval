060115d20effae4f67b120daf65147277146ae02
mel-oezkan <melih.oezkan@stud.tu-darmstadt.de>
2025-11-10 12:02:49 +0100

    chore: added apn script and corrected n_attriutes

---
+++ CUB/scripts/run_apn.sh
@@ -0,0 +1,18 @@
+python experiments.py cub Joint \
+    --seed 1 \
+    -ckpt 1 \
+    -log_dir outputs/ \
+    -e 1000 \
+    -optimizer sgd \
+    -pretrained \
+    -use_aux \
+    -use_attr \
+    -weighted_loss multiple \
+    -data_dir data/CUB_processed/class_attr_data_10 \
+    -n_attributes 112 \
+    -attr_loss_weight 1 \
+    -normalize_loss -b 64 \
+    -weight_decay 0.00004 \
+    -lr 0.01 \
+    -scheduler_step 1000 \
+    -end2end
