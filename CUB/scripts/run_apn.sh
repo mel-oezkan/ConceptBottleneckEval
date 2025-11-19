95ecf726ad48180e44586190b707fb6aef73504e
Melih <melih.oezkan@stud.tu-darmstadt.de>
2025-11-19 21:16:58 +0100

    script for apn

---
+++ CUB/scripts/run_apn.sh
@@ -0,0 +1,18 @@
+
+python experiments.py cub APN \
+    --seed 1 \
+    -ckpt "" \
+    -log_dir outputs/test1 \
+    -e 1000 \
+    -optimizer sgd \
+    -pretrained \
+    -use_aux \
+    -weighted_loss multiple \
+    -data_dir data/CUB_processed/class_attr_data_10 \
+    -n_attributes 112 \
+    -attr_loss_weight 1 \
+    -normalize_loss -b 64 \
+    -weight_decay 0.00004 \
+    -lr 0.01 \
+    -scheduler_step 1000 \
+    -end2end
