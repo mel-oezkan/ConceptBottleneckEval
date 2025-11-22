68a3a6f238c65001173736940ab823de918395aa
Sebastian Bock <sebastian.bock@stud.tu-darmstadt.de>
2025-11-22 13:16:51 +0100

    Implement mIoU computation for each part segmentation

--- APN/index_translation_util.py
+++ APN/index_translation_util.py
@@ -16,7 +16,7 @@ CUB_SELECTED_ATTRIBUTES_PER_GROUP = {
 # print(CUB_SELECTED_ATTRIBUTES_PER_GROUP)
 
 
-def map_attribute_ids_to_part_seg_group_id(batch_size):
+def map_attribute_ids_to_part_seg_group_id(batch_size, verbose=True):
     """
         Creates a lookup tensor that, given the index of an attribute within our model output, maps it to its original attribute
         ID, maps it to the selected ones, and then maps it to the ID of its corresponding part segmentation group.
@@ -26,7 +26,7 @@ def map_attribute_ids_to_part_seg_group_id(batch_size):
     attr_to_group = {}
     for group_id, group_name in enumerate(PART_SEG_GROUPS):
         for idx in CUB_GROUPS_TO_PART_SEG[group_name]:
-            if idx in attr_to_group:
+            if verbose and idx in attr_to_group:
                 print(f"Attribute ID {idx} is assigned to multiple groups: Old group = {attr_to_group[idx]}, New group = {group_id}")
             attr_to_group[idx] = group_id
 
@@ -37,7 +37,8 @@ def map_attribute_ids_to_part_seg_group_id(batch_size):
 
         # This happens because of the "other" group, fill with dummy value, remove in main script from attention map!
         if CUB_SELECTED_ATTRIBUTES[attr_idx] not in attr_to_group.keys():
-            print(f"Attribute with ID {CUB_SELECTED_ATTRIBUTES[attr_idx]} could not be matched to any part segmentation group.")
+            if verbose:
+                print(f"Attribute with ID {CUB_SELECTED_ATTRIBUTES[attr_idx]} could not be matched to any part segmentation group.")
             unmatched_indices.append(attr_idx)
             continue
 
@@ -48,9 +49,8 @@ def map_attribute_ids_to_part_seg_group_id(batch_size):
     mask[unmatched_indices] = False
     lookup_clean = lookup[mask]
 
-    # Batch it to enable efficient lookup
     # This is our final tensor that maps each attribute by its id in the attention map to the respective part seg group
-    return lookup_clean.unsqueeze(0).expand(batch_size, -1), mask
+    return lookup_clean, mask
 
 
 def get_attribute_names(path_to_cub_data, used_attributes_only=True):
