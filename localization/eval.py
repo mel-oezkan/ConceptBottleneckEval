676eda21aa6b5989d7da47118eda5ec06a358e41
MarcSaghir <marc.saghir@gmail.com>
2025-11-24 16:02:01 +0100

    untested but commitworthy locAcc

--- localization/eval.py
+++ localization/eval.py
@@ -11,7 +11,7 @@ from CUB.dataset import CUBDataset
 from utils import get_KP_BB, get_iou
 
 
-#rewrite of method because this sucks
+#TO-DO: CUB parts to Sebbis seg groups mapping einbauen
 def test_CUB_IoU(args:dict, model, dataset:CUBDataset, CUB_root:str, part_attribute_mapping:dict):
     # part_attribute_mapping means a dict that maps from a part (CUB/parts/parts.txt) to a list of attribute IDs
     # sum of all attribute IDs must not be more that number of attention maps returned/attributes used by model. also is
@@ -121,7 +121,8 @@ def test_CUB_IoU(args:dict, model, dataset:CUBDataset, CUB_root:str, part_attrib
     body_avg_IoU, mean_IoU = calculate_average_partwise_acc(whole_IoU, IoU_thr=args.IoU_thr)
     return body_avg_IoU, mean_IoU
 
-
+#import part mapping
+#TO-DO: change code so that it merges all parts with the same keys for a given part mapping
 def calculate_average_partwise_acc(all_ious:list[dict], IoU_thr: float=0.5):
     #compute acc with ious and threshold for all images per part
     #all ious = list with each item having a matching from part to iou
@@ -220,137 +221,6 @@ def get_CUB_paths(CUB_root:str):
     bird_BB_path = os.path.join(CUB_root, "bounding_boxes.txt")
 
     return imgID_imgName_mapping_path, parts_locs_path, parts_mapping_path, bird_BB_path
-
-
-
-
-
-def test_with_IoU(opt, model, testloader, vis_groups=None, vis_root=None,
-                  required_num=2, save_att=False, sub_group_dic=None, group_dic=None):
-    """
-    save feature maps to visualize the activation
-    :param model: loaded model
-    :param testloader:
-    #:param attribute: test attributes (model input, no effect to activation maps here)
-    #:param test_classes: test classes (accuracy input, no effect to activation maps here)
-    :param vis_groups: the groups to be shown
-    :param vis_layer: the layers to be shown
-    :param vis_root: save path to activation maps
-    :param required_num: the number images shown in each categories
-    :return:
-    """
-    # print('Calculating the IoU of attention maps, saving attention map to:', save_att)
-    layer_name = 'layer4'
-    #GT_targets = []
-    #predicted_labels = []
-    vis_groups = group_dic
-
-    whole_IoU = []
-    # print("vis_groups:", vis_groups)
-    # required_num = 2  # requirement denotes the number for each categories
-    with torch.no_grad():
-        count = dict()
-        for i, (input, target, impath) in \
-                enumerate(testloader):
-            save_att_idx = []
-            labels = target.data.tolist()
-            for idx in range(len(labels)):
-                label = labels[idx]
-                if label in count:
-                    count[label] = count[label] + 1
-                else:
-                    count[label] = 1
-                if count[label] <= required_num:
-                    save_att_idx.append(1)
-                else:
-                    save_att_idx.append(0)
-
-            if opt.cuda:
-                input = input.cuda()
-                target = target.cuda()
-
-            output, pre_attri, attention, _ = model(input)#, attribute)
-            # pre_attri.shape : 64，112
-            # attention.shape : 64, 112, 8, 8
-            maps = {layer_name: attention['layer4'].cpu().numpy()}
-            pre_attri = pre_attri['layer4']
-            target_groups = [{} for _ in range(output.size(0))]  # calculate the target groups for each image
-            # target_groups is a list of size image_num
-            # each item is a dict, including the attention index for each subgroup
-            for part in vis_groups.keys():
-                sub_group = sub_group_dic[part]
-                keys = list(sub_group.keys())
-
-                # sub_activate_id is the attention id for each part in each image. The size is img_num * sub_group_num
-                sub_activate_id = []
-                for k in keys:
-                    sub_activate_id.append(torch.argmax(pre_attri[:, sub_group[k]], dim=1, keepdim=True))
-                sub_activate_id = torch.cat(sub_activate_id, dim=1).cpu().tolist()  # (batch_size, sub_group_dim)
-                for attention_id, argdims in enumerate(sub_activate_id):
-                    target_groups[attention_id][part] = [sub_group[keys[i]][argdim] for i, argdim in enumerate(argdims)]
-
-            KP_root = './data/vis/save_KPs/'
-            scale = opt.IoU_scale
-            batch_IoU = calculate_atten_IoU(input, impath, save_att_idx, maps, [layer_name], target_groups, KP_root,
-                                            save_att=save_att, scale=scale, resize_WH=opt.resize_WH,
-                                            KNOW_BIRD_BB=opt.KNOW_BIRD_BB)
-            # print('batch_IoU:', batch_IoU)
-            whole_IoU += batch_IoU
-            #_, predicted_label = torch.max(output.data, 1)
-            #predicted_labels.extend(predicted_label.cpu().numpy().tolist())
-            #GT_targets = GT_targets + target.data.tolist()
-            # break
-
-    body_avg_IoU, mean_IoU = calculate_average_IoU(whole_IoU, IoU_thr=opt.IoU_thr)
-    #GT_targets = np.asarray(GT_targets)
-    #acc_all, acc_avg = compute_per_class_acc(map_label(torch.from_numpy(GT_targets), test_classes).numpy(),
-    #                                 np.array(predicted_labels), test_classes.numpy())
-    return body_avg_IoU, mean_IoU
-
-
-
-def map_label(label, classes):
-    mapped_label = torch.LongTensor(len(label))
-    for i in range(classes.size(0)):
-        mapped_label[label==classes[i]] = i    
-    return mapped_label
-
-
-def compute_per_class_acc(test_label, predicted_label, nclass):
-    test_label = np.array(test_label)
-    predicted_label = np.array(predicted_label)
-    acc_per_class = []
-    acc = np.sum(test_label == predicted_label) / len(test_label)
-    for i in range(len(nclass)):
-        idx = (test_label == i)
-        acc_per_class.append(np.sum(test_label[idx] == predicted_label[idx]) / np.sum(idx))
-    return acc, sum(acc_per_class)/len(acc_per_class)
-
-
-
-def calculate_average_IoU(whole_IoU, IoU_thr=0.5):
-    img_num = len(whole_IoU)
-    body_parts = whole_IoU[0].keys()
-    body_avg_IoU = {}
-    for body_part in body_parts:
-        body_avg_IoU[body_part] = []
-        body_IoU = []
-        for im_id in range(img_num):
-            if len(whole_IoU[im_id][body_part]) > 0:
-                if_one = []
-                for item in whole_IoU[im_id][body_part]:
-                    if_one.append(1 if item > IoU_thr else 0)
-                body_IoU.append(mean(if_one))
-        body_avg_IoU[body_part].append(mean(body_IoU))
-    num = 0
-    sum = 0
-    for part in body_avg_IoU:
-        if part != 'tail':
-            sum += body_avg_IoU[part][0]
-            num += 1
-    # print(sum/num *100)
-    return body_avg_IoU, sum/num *100
-
 
 
 
--- localization/utils.py
+++ localization/utils.py
@@ -3,115 +3,6 @@ import numpy as np
 import os
 import cv2
 
-def calculate_atten_IoU(input, impath, save_att_idx, maps, names, vis_groups, KP_root=None, save_att=False, scale=4,
-                        resize_WH=False, KNOW_BIRD_BB=False):
-    """
-    :param input: input image
-    :param impath: image paths
-    :param maps: maps with size 64, 312, 7, 7
-    :param names: layer names
-    :param vis_groups: vis_groups is a list of size image_num. each item is a dict,
-                       including the attention index for each subgroup
-    :param KP_root: is the root of KP_centers
-    :return:
-    """
-    # print("resize_WH:{}, out_of_edge:{}, max_area_center:{}".format(resize_WH, out_of_edge, max_area_center))
-    img_raw_show = tensor_imshow(input)  # (64, 3, 224, 224)
-    attri_names = refine_attri_names('./data/vis/files/attri_name.txt')
-    batch_IoU = []
-    for i, path in enumerate(impath):
-        # time_1 = time.time()
-        # print(i, path)
-        if type(vis_groups) is dict:
-            vis_group = vis_groups
-        elif type(vis_groups) is list:
-            vis_group = vis_groups[i]
-        else:
-            exit('ERROR FOR vis_groups!')
-        tmp = path.split('/')[-2:]
-        this_dir = os.path.join(KP_root, tmp[0], tmp[1][:-4])
-        if save_att:
-            save_dir = os.path.join(save_att, tmp[0], tmp[1][:-4])
-        else:
-            save_dir = False
-        # KP_BBs, bird_BB = read_BB(this_dir)
-        KPs, bird_BB = read_KP(this_dir)
-        # print("this dir:", this_dir)
-        # time_2 = time.time()
-        # print('time for load BB:', time_2 - time_1)
-        img_IoU = get_IoU_Image(i, img_raw_show, maps, save_dir, save_att_idx, names, vis_group, attri_names,
-                                KPs, bird_BB, scale, resize_WH, KNOW_BIRD_BB)
-        # time_3 = time.time()
-        # print('time for calculate IoU:', time_3 - time_2)
-        batch_IoU.append(img_IoU)
-        # exit()
-    return batch_IoU
-
-
-
-def get_IoU_Image(idx, imgs, maps, save_dir, save_att_idx, names, groups,
-                  attri_names, KPs, bird_BB, scale, resize_WH, KNOW_BIRD_BB):
-    BB_parts = ['head', 'breast', 'belly', 'back', 'wing', 'tail', 'leg']
-    # {'x1': KP_x1, 'x2': KP_x2, 'y1': KP_y1, 'y2': KP_y2}
-    attri_to_group_dict = {
-        'head': [1, 4, 5, 6, 9, 10],
-        'breast': [3, 14],
-        'belly': [2],
-        'back': [0],
-        'wing': [8, 12],
-        'tail': [13],
-        'leg': [7, 11],
-    }
-
-    bird_w = bird_BB[2] - bird_BB[0]
-    bird_h = bird_BB[3] - bird_BB[1]
-    mask_w = int(bird_w / scale)
-    mask_h = int(bird_h / scale)
-
-    if resize_WH:
-        mask_h = max(int(mask_w / 2), mask_h)
-        mask_w = max(int(mask_h / 2), mask_w)
-
-    img_IoU = {}
-    for group_name, group_dims in groups.items():
-        # print("group_name, group_dims:", group_name, group_dims)
-        if group_name == 'others':
-            continue
-
-        img_IoU[group_name] = []
-        # if the body part exist:
-        # generate the iou for each attention map from each subgroup
-        for group_dim in group_dims:
-            for j, name in enumerate(names):
-                mask = maps[name][idx, group_dim, :, :]
-                mask = cv2.resize(mask, (224, 224))
-                mask_BB_dict, (mask_c_x, mask_c_y) = generate_mask_BB(mask, bird_BB, scale, KNOW_BIRD_BB)
-
-                KP_idxs = attri_to_group_dict[group_name]
-                KPs_sub = [KPs[KP_idx][:2] for KP_idx in KP_idxs if KPs[KP_idx][2] != 0]
-                if len(KPs_sub) == 0:
-                    continue
-                dis = [(point[0] - mask_c_x) ** 2 + (point[1] - mask_c_y) ** 2 for point in KPs_sub]
-                gt_point = KPs_sub[np.argmin(dis)]
-                KP_BB_dict = get_KP_BB(gt_point, mask_h, mask_w, bird_BB, KNOW_BIRD_BB)
-                # print("KP_BB_dict", KP_BB_dict, mask_BB_dict)
-                IoU = get_iou(KP_BB_dict, mask_BB_dict)
-                img_IoU[group_name].append(IoU)
-    return img_IoU
-
-
-def generate_mask_BB(mask, bird_BB, scale=4, KNOW_BIRD_BB=False):
-    """
-    :return   a dict {'x1', 'x2', 'y1', 'y2'}
-    """
-    bird_w = bird_BB[2] - bird_BB[0]
-    bird_h = bird_BB[3] - bird_BB[1]
-    mask_w = int(bird_w / scale)
-    mask_h = int(bird_h / scale)
-    # np. np.max(mask)
-    (mask_c_x, mask_c_y) = np.unravel_index(np.argmax(mask), np.array(mask).shape)
-    mask_BB = get_KP_BB((mask_c_x, mask_c_y), mask_h, mask_w, bird_BB, KNOW_BIRD_BB)
-    return mask_BB, (mask_c_x, mask_c_y)
 
 
 def get_KP_BB(gt_point, mask_h, mask_w, bird_BB, KNOW_BIRD_BB=False):
@@ -134,33 +25,6 @@ def get_KP_BB(gt_point, mask_h, mask_w, bird_BB, KNOW_BIRD_BB=False):
         KP_y1, KP_y2 = Bound[3] - mask_h, Bound[3]
     return {'x1': KP_x1, 'x2': KP_x2, 'y1': KP_y1, 'y2': KP_y2}
 
-
-
-def read_KP(dir):
-    KPs = np.load(dir + '.npy')
-    bird_BB = np.load(dir + '_bird_BB.npy')
-    # print("KPs.shape:", KPs.shape)
-    # print("bird_BB.shape:", bird_BB.shape)
-    return KPs, bird_BB
-
-
-def tensor_imshow(inp):
-    """Imshow for Tensor."""
-    # inp = inp.cpu().numpy().transpose((1, 2, 0))
-    inp = inp.detach().squeeze().cpu().numpy()
-    # print("inp.shape:", inp.shape)
-    # Mean and std for ImageNet
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    for i in range(3):
-        inp[:, i] = inp[:, i] * std[i] + mean[i]
-    # inp = std * inp + mean
-    # inp = np.clip(inp, 0, 1)
-    # plt.imshow(inp, **kwargs)
-    # if title is not None:
-    #     plt.title(title)
-    # print(inp.shape)
-    return inp
 
 
 def get_iou(bb1, bb2):
@@ -211,15 +75,6 @@ def get_iou(bb1, bb2):
     iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
     assert iou >= 0.0 and iou <= 1.0
     return iou
-
-
-
-def refine_attri_names(fn):
-    attri_names = open(fn).readlines()
-    assert len(attri_names) == 312
-    for i in range(len(attri_names)):
-        attri_names[i] = attri_names[i].strip().replace('::', '_').replace('(', '').replace(')', '')
-    return attri_names
 
 
 
